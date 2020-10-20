017466d655c6423e08431e3e17a004abcf31b4ca
Roy Nunez <roynunez273@gmail.com>
2020-10-19 22:08:21 -0700

    Sound

---
+++ PianoWorking/MusicNote.js
@@ -0,0 +1,15 @@
+class MusicNote{
+    constructor(){
+        this.text1 = "tt oo pp o ii uu yy t oo ii uu y oo ii uu y tt oo pp o ii uu yy t"
+        var note = [text1];
+    }
+    display(){
+        random
+    }
+}
+// setSong("Twinkle Twinkle Little Star", "tt oo pp o ii uu yy t oo ii uu y oo ii uu y tt oo pp o ii uu yy t", 1);
+// setSong("He's A Pirate", "psdd dfgg ghff dssd psdd dfgg ghff dsd ", 2);
+// setSong("Fur Elise", "f D f D f a d s p  t u p a  u O a s  u f D f D f a d s  p  t u p a   u s a p", 3);
+// setSong("Harry Potter Theme", "a f h G f a p G  f h G d g a   a f h G f a d S s  p s a P t o u", 4);
+// setSong("Star Spangled Banner", "o u t u o s f d s u I o o o f d s a p a s s o u t o u t u o s f d s u I o o o f d s a p a s s o u t f f f g h h g f d f g g g f d s a p a s s u t o s s s a p p p d g f d s a o o s d f g h s d f g d s s", 5);
+// setSong("Indiana Jones Theme", "u i o s  y u i o p a g p a s d f u i o s d f g o o f d o f d o f d o f d  u i o s y u i  o p a g p a s d f   u i o s  d f g   o o f d o f d o f d o f d   ", 6);
